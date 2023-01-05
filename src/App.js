3e9d2f7128b32b7b2803ae6fbb4bafcf4caac2b2
Revati <revati.v.bordawekar@gmail.com>
2023-01-05 09:16:33 -0800

    template for toggling states in React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Items } from './components/Items';
+
+function App() {
+  return (
+    <div className="App">
+      <Items />
+    </div>
+  );
+}
+
+export default App;
